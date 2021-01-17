1634e8eb94bde84c37dae9aba764269fc72202be
shoxie <sktt1lka@gmail.com>
2021-01-17 09:28:08 +0700

    lmao

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "Nasalization": ["Nasalization", "sans-serif"],
+      },
+      colors: {
+        mygreen: "#44D62C",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
